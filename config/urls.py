9dade59318f3adb6468a4fc6d4721a5d45a3fcf7
Sigby23 <signyv24@ru.is>
2026-05-04 13:24:01 +0000

    logi/signup

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("artvault.urls")),
     path("admin/", admin.site.urls),
+    path("user/", include("user.urls")),
 ]
--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', LoginView.as_view('user/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
