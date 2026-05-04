a811987a505d88bb6b0fb88dfc3dffa1d971358b
Steinar Helgi <steinarhelgi@outlook.com>
2026-05-04 13:23:08 +0000

    static file directory

--- artvault/urls.py
+++ artvault/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="artvault-index"),
+    path("steinar_test", views.steinar_test, name="artvault-steinar_test"),
     path("<int:id>", views.get_art_by_id, name="artvault-index"),
 ]
