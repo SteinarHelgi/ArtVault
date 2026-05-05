cdeb49bb31b7fc50ae9d3ede141efe298a318b9e
robotlover123 <thordis886@gmail.com>
2026-05-05 11:25:11 +0000

    js

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function MyDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.closest(".dropdown")) {
+      document.getElementById("myDropdown").classList.remove("show");
+    }
+  }
