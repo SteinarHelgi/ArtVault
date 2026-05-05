72b5c70694ab1896c70eca3c90578fd3286ce096
Steinar Helgi <steinarhelgi@outlook.com>
2026-05-05 12:42:32 +0000

    database: tengja

--- artvault/apps.py
+++ artvault/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ArtvaultConfig(AppConfig):
-    name = 'artvault'
+    name = "artvault"
--- artvault/models.py
+++ artvault/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from user.models import SellerProfileModel
+
 # Create your models here.
+
+
+class Artwork(models.Model):
+    seller = models.ForeignKey(SellerProfileModel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    medium = models.CharField(max_length=255)
+    edition = models.CharField(max_length=255)
+    dimensions = models.CharField(max_length=255)
+    sold = models.BooleanField()
+    history = models.CharField(max_length=4096)
+    artist_name = models.CharField(max_length=255)
+    date = models.DateField()
+    starting_price = models.CharField(max_length=255)
+    art_movement = models.CharField(max_length=255)
+
+
+class ArtWorkImage(models.Model):
+    image_path = models.CharField(max_length=255)
+    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
