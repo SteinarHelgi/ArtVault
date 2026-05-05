f85d16306917de4f04c981457ef3ff3177323a11
robotlover123 <thordis886@gmail.com>
2026-05-05 12:24:44 +0000

    laga body css

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 //dropdown menu
-function MyDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+const MyDropdown = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+};
 
-  window.onclick = function(event) {
-    if (!event.target.closest(".dropdown")) {
-      document.getElementById("myDropdown").classList.remove("show");
-    }
+window.onclick = (event) => {
+  if (!event.target.closest(".dropdown")) {
+    document.getElementById("myDropdown").classList.remove("show");
   }
+};
 
   // close button á popup
   function closeModal() {
