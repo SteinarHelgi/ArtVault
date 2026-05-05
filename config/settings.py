c107a4269df81dd6ba53103855f7f1750e28004d
Steinar Helgi <steinarhelgi@outlook.com>
2026-05-05 10:33:40 +0000

    config og allt saman

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
+import os
 
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+SERVER_PASSWORD = os.getenv("SERVER_PASSWORD")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +84,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "verklegt_namskeid_db",
+        "USER": "steinar",
+        "PASSWORD": SERVER_PASSWORD,
+        "HOST": "db-verklegt-steinar-789abc.postgres.database.azure.com",
+        "PORT": "5432",
     }
 }
 
