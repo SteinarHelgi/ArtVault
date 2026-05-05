415ce6d1d366133a5b75b4beee4d9a6b58363576
Steinar Helgi <steinarhelgi@outlook.com>
2026-05-05 09:22:46 +0000

    templates virka og allt saman

--- artvault/urls.py
+++ artvault/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('artwork_details/<int:id>', views.artwork_details, name='artwork-details'),
     path("", views.index, name="artvault-index"),
-    path("steinar_test/", views.steinar_test, name="artvault-steinar_test"),
-    path("<int:id>", views.get_art_by_id, name="artvault-index"),
+    path("artwork_details/<int:id>", views.artwork_details, name="artwork-details"),
 ]
--- artvault/views.py
+++ artvault/views.py
@@ -5,18 +5,11 @@ from .data import artworks
 
 # Create your views here
 def index(request):
-    return render(request, "base.html", {"art": artworks})
-
-
-def get_art_by_id(request, id):
-    return HttpResponse(f"Hi there! {request.path},with ID {id}")
+    return render(request, "artvault/index.html", {"art": artworks})
 
 
 def blabla(request): ...
 
 
-def steinar_test(request):
-    return render(request, "artvault/steinar_test.html", {"art": artworks})
-
 def artwork_details(request, id):
-    return render(request, 'artvault/artwork_details.html', {"art": artworks, "id": id})
+    return render(request, "artvault/artwork_details.html", {"art": artworks, "id": id})
