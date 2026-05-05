c7e41ba6021926f2b0abcdb5ea0fd195081ab2a4
atlivar <atlivar2000@gmail.com>
2026-05-05 13:14:45 +0000

    browse_artwork.html

--- artvault/urls.py
+++ artvault/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("browse_artwork/", views.browse_artwork, name="artvault-browse-artwork"),
     path(
-        "artwork_details/<int:id>",
+        "artwork_details/<int:id>/",
         views.artwork_details,
         name="artvault-artwork-details",
     ),
