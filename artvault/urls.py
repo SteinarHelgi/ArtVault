d99ba81b5dd8e3cbcf1f3b1b05f888598c82d14c
Steinar Helgi <steinarhelgi@outlook.com>
2026-05-05 11:18:34 +0000

    laga

--- artvault/urls.py
+++ artvault/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="artvault-index"),
+    path("", views.index, name="home"),
     path("browse_artwork/", views.browse_artwork, name="artvault-browse-artwork"),
-    path("artwork_details/<int:id>", views.artwork_details, name="artvault-artwork-details"),
+    path(
+        "artwork_details/<int:id>",
+        views.artwork_details,
+        name="artvault-artwork-details",
+    ),
 ]
