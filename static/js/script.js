9d00f62abdb472cbdc06ecf02513a2f8bfd019b4
robotlover123 <thordis886@gmail.com>
2026-05-06 17:53:37 +0000

    landing page pretty

--- static/js/script.js
+++ static/js/script.js
@@ -16,7 +16,7 @@ window.addEventListener("click", function (event) {
   if (!event.target.closest(".profile-dropdown")) {
     document.getElementById("rightDropdown")?.classList.remove("show");
   }
-};
+});
 
   // close button á popup
   function closeModal() {
@@ -24,3 +24,31 @@ window.addEventListener("click", function (event) {
   }
 
 
+  // searchbar filtering fyrir browse artwork
+function filterFunction() {
+    let input = document.getElementById("searchbar");
+    let filter = input.value.toUpperCase();
+
+    let cards = document.getElementsByClassName("browse-art-link");
+
+    for (let i = 0; i < cards.length; i++) {
+        let cardText = cards[i].innerText.toUpperCase();
+
+        if (cardText.indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+//scroll func on landing page
+function scrollArt(button, direction) {
+    const wrapper = button.closest(".art-scroll-wrapper");
+    const container = wrapper.querySelector(".art-movement-container");
+
+    container.scrollBy({
+        left: direction * 300,
+        behavior: "smooth"
+    });
+}
