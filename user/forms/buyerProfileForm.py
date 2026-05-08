0245f5ea08327bf62b16659cd6fd148f19c4502b
Sigby23 <signyv24@ru.is>
2026-05-08 12:35:59 +0000

    verður að fylla út fields í setup, kemur message

--- user/forms/buyerProfileForm.py
+++ user/forms/buyerProfileForm.py
@@ -3,10 +3,11 @@ from django.forms import ModelForm
 from user.models import BuyerProfileModel
 
 class BuyerProfileForm(forms.ModelForm):
+    profile_image = forms.ImageField(required=True, widget= forms.ClearableFileInput(attrs={'class': 'hidden-file-input','onchange': 'previewBuyerProfileImage(event)'}))
+
     class Meta:
         model = BuyerProfileModel
         exclude = ('user', 'id', 'profile')
         widgets = {
             'full_name': forms.TextInput(attrs={'class': 'profile-input', 'placeholder': 'Full Name'}),
-            'profile_image': forms.ClearableFileInput(attrs={'class': 'hidden-file-input','onchange': 'previewBuyerProfileImage(event)'}),
         }
