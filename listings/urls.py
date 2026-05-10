bd6fb00edab29525017bda88d13bd9b46159c94e
Steinar Helgi <steinarhelgi@outlook.com>
2026-05-10 13:40:35 +0000

    added the bids to the my listings page
    Now the seller can see the bids he has on this artwork

--- listings/urls.py
+++ listings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("add_listing/", views.add_listing_view, name="add_listing"),
+    path("my_listing/<int:id>", views.my_listing_view, name="my-listing"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.http import HttpResponseForbidden
 from django.shortcuts import redirect, render
 
-from artvault.models import ArtworkImage
+from artvault.models import Artwork, ArtworkImage
 from listings.forms.listing_form import ArtworkListingForm
 
 # Create your views here.
@@ -36,5 +36,7 @@ def add_listing_view(request):
     return render(request, "listings/add_listing.html", {"form": form})
 
 
-# def my_listings_view(request):
-#     profile = user.profile.sellerprofilemodel
+def my_listing_view(request, id):
+    artwork = Artwork.objects.get(pk=id)
+    bids = artwork.bids.all().order_by('-amount')
+    return render(request, "listings/my_listing.html", {"artwork": artwork,"bids": bids})
