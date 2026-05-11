d3ae108402459b678d998e11fb941215bd985491
atlivar <atlivar2000@gmail.com>
2026-05-11 13:02:26 +0000

    remove listing

--- listings/urls.py
+++ listings/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("add_listing/", views.add_listing_view, name="add_listing"),
     path("my_listing/<int:id>", views.my_listing_view, name="my-listing"),
+    path("delete_listing/<int:id>", views.delete_listing_view, name="delete-listing"),
 ]
