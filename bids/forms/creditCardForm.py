5adb4739d6296d6393c874ec9804f2d12b1e53fb
Sigby23 <signyv24@ru.is>
2026-05-14 11:53:24 +0000

    laga mm yy format í credit card og setja minimum bid i input kassan hjá bids

--- bids/forms/creditCardForm.py
+++ bids/forms/creditCardForm.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.forms import ModelForm
 from bids.models import CreditCardModel
-
+from django.core.validators import RegexValidator
+expiry_validator = RegexValidator(
+    regex=r"^(0[1-9]|1[0-2])\/\d{2}$",
+    message="Enter expiry date as MM/YY"
+)
 class CreditCardForm(ModelForm):
+    card_expiration = forms.CharField(validators=[expiry_validator],widget=forms.TextInput(attrs={"placeholder": "MM/YY"}))
+
     class Meta:
         model = CreditCardModel
         fields = ['cardholder_name', 'card_number', 'card_cvv', 'card_expiration']
@@ -10,5 +16,4 @@ class CreditCardForm(ModelForm):
             'cardholder_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Full Name'}),
             'card_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '0000-0000-0000-0000'}),
             'card_cvv': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '000'}),
-            'card_expiration': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'MM/YY'}),
         }
