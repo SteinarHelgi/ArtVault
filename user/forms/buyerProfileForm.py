e9418f2444aeb212b2383431e1e4c00f248c4c6f
atlivar <atlivar2000@gmail.com>
2026-05-15 12:07:37 +0000

    laga error field í setup

--- user/forms/buyerProfileForm.py
+++ user/forms/buyerProfileForm.py
@@ -1,13 +1,35 @@
 from django import forms
-from django.forms import ModelForm
 from user.models import BuyerProfileModel
 
+
 class BuyerProfileForm(forms.ModelForm):
-    profile_image = forms.ImageField(required=True, widget= forms.ClearableFileInput(attrs={'class': 'hidden-file-input','onchange': 'previewBuyerProfileImage(event)'}))
+
+    profile_image = forms.ImageField(
+        required=False,
+        error_messages={
+            "required": "Please upload a profile image."
+        },
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'hidden-file-input',
+            'onchange': 'previewBuyerProfileImage(event)'
+        })
+    )
 
     class Meta:
         model = BuyerProfileModel
         exclude = ('user', 'id', 'profile')
+
         widgets = {
-            'full_name': forms.TextInput(attrs={'class': 'profile-input', 'placeholder': 'Full Name'}),
+            'full_name': forms.TextInput(attrs={
+                'class': 'profile-input',
+                'placeholder': 'Full Name'
+            }),
         }
+
+    def clean_profile_image(self):
+        profile_image = self.cleaned_data.get("profile_image")
+
+        if not profile_image:
+            raise forms.ValidationError("Please upload a profile image.")
+
+        return profile_image
--- user/forms/sellerProfileForm.py
+++ user/forms/sellerProfileForm.py
@@ -1,11 +1,37 @@
 from django import forms
 from django.forms import ModelForm
 from user.models import SellerProfileModel
-from user.models import Profile
 
 class SellerProfileForm(ModelForm):
-    logo = forms.ImageField(required=True, widget=forms.ClearableFileInput(attrs={'class': 'hidden-file-input', 'onchange': 'previewLogo(event)' }))
-    cover_photo = forms.ImageField(required=True, widget=forms.ClearableFileInput(attrs={'class': 'hidden-file-input','onchange': 'previewCover(event)'}))
+    logo = forms.ImageField(
+        required=False,
+        error_messages={
+            "required": "Please upload a logo."
+        },
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'hidden-file-input',
+            'onchange': 'previewLogo(event)'
+        })
+    )
+
+    cover_photo = forms.ImageField(
+        required=False,
+        error_messages={
+            "required": "Please upload a cover photo."
+        },
+        widget=forms.ClearableFileInput(attrs={
+            'class': 'hidden-file-input',
+            'onchange': 'previewCover(event)'
+        })
+    )
+    zip_code = forms.RegexField(regex=r'^\d{3}$',
+        error_messages={
+            "invalid": "Postal code must be exactly 3 numbers."
+        },
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Postal code'
+        })
+    )
     class Meta:
         model = SellerProfileModel
         exclude = ('user', 'id', 'profile')
@@ -13,9 +39,24 @@ class SellerProfileForm(ModelForm):
             'seller_name': forms.TextInput(attrs={'placeholder': 'Full name'}),
             'street_name': forms.TextInput(attrs={'placeholder': 'Street name'}),
             'city': forms.TextInput(attrs={'placeholder': 'City'}),
-            'zip_code': forms.TextInput(attrs={'placeholder': 'Postal code'}),
             'logo': forms.ClearableFileInput(attrs={'class': 'hidden-file-input', 'onchange': 'previewLogo(event)' }),
             'cover_photo': forms.ClearableFileInput(attrs={'class': 'hidden-file-input','onchange': 'previewCover(event)'}),
             'bio': forms.Textarea(attrs={'placeholder': 'About me...'}),
         }
+
+    def clean_logo(self):
+        logo = self.cleaned_data.get("logo")
+
+        if not logo:
+            raise forms.ValidationError("Please upload a logo.")
+
+        return logo
+
+    def clean_cover_photo(self):
+        cover_photo = self.cleaned_data.get("cover_photo")
+
+        if not cover_photo:
+            raise forms.ValidationError("Please upload a cover photo.")
+
+        return cover_photo
 
