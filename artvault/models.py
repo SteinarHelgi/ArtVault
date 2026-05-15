8f153dfc23f570e6e68f48860a88befe29fffd2a
Sigby23 <signyv24@ru.is>
2026-05-15 14:36:18 +0000

    bæta við date of bid

--- artvault/models.py
+++ artvault/models.py
@@ -48,6 +48,7 @@ class Bid(models.Model):
     status = models.CharField(max_length=255)
     buyer = models.ForeignKey(BuyerProfileModel, on_delete=models.CASCADE)
     artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE, related_name="bids")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     @property
     def formatted_amount(self):
